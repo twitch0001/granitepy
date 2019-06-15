78996c61028274013b1227e7b9498629a11294ed
twitch <rocharod7@gmail.com>
2019-06-15 14:37:29 -0700

    Added filter import

--- andesite/__init__.py
+++ andesite/__init__.py
@@ -6,6 +6,7 @@ Made by twitch#7443
 from .client import Client
 from .events import *
 from .exceptions import *
+from .filters import *
 from .objects import Track
 from .player import Player
 from .websocket import WebSocket
