d9651840dcd9362994cc6046471fa1b78a361e8b
twitch <rocharod7@gmail.com>
2019-08-11 16:47:53 +0100

    Ver bump for update

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ with open("README.md", "r") as fh:
 
 
 setup(
-    name = "granitepy",
-    packages = ['andesite'],
-    version = "0.0.19a",
-    description = "A library for the lavalink like audio provider called andesite",
+    name="granitepy",
+    packages=['andesite'],
+    version="0.0.20a",
+    description="A library for the lavalink like audio provider called andesite",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author = "twitch#7443",
-    author_email = "twitch@trenchbot.xyz",
-    url = "https://github.com/twitch0001/granitepy",
-    keywords = ['andesite'],
+    author="twitch#7443",
+    author_email="twitch@trenchbot.xyz",
+    url="https://github.com/twitch0001/granitepy",
+    keywords=['andesite'],
     install_requires=['websockets>=6.0,<7.0', 'aiohttp', 'discord.py'],
-    classifiers = [
+    classifiers=[
         "Framework :: AsyncIO",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
